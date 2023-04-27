ea08d6b8d6aaf73f144a3068eeae40c0562fc261
Hao-Xin Lin <43382049+HxLyn3@users.noreply.github.com>
2023-04-27 14:30:33 +0800

    add operations with scalar

--- matrix.hpp
+++ matrix.hpp
@@ -18,15 +18,15 @@ class Matrix
         Matrix(const initializer_list<initializer_list<float>> &);
 
         // shape
-        int *shape(){
+        int *shape() {
             static int shape[2];
-            shape[0] = this->M;
-            shape[1] = this->N;
+            shape[0] = M;
+            shape[1] = N;
             return shape;
         };
 
         // assignment
-        Matrix &operator=(Matrix &) = default;
+        Matrix &operator=(const Matrix &) = default;
         Matrix &operator=(const initializer_list<initializer_list<float>> &);
 
         // index
@@ -34,24 +34,55 @@ class Matrix
         vector<float> &row(int r);
         vector<float> col(int c);
 
+        // transpose
+        Matrix transpose();
+
         // concatenate
-        Matrix cat(Matrix &, int);
+        Matrix cat(Matrix, int);
 
         // print
         friend ostream &operator<<(ostream &out, Matrix);
 
         // addition and subtraction
-        Matrix operator+(Matrix &);
-        void operator+=(Matrix &);
-        Matrix operator-(Matrix &);
-        void operator-=(Matrix &);
+        Matrix operator+(Matrix);
+        void operator+=(Matrix);
+        Matrix operator-(Matrix);
+        void operator-=(Matrix);
 
-        // element-wise multiplication
-        Matrix operator*(Matrix &);
-        void operator*=(Matrix &);
+        // element-wise multiplication and division
+        Matrix operator*(Matrix);
+        void operator*=(Matrix);
+        Matrix operator/(Matrix);
+        void operator/=(Matrix);
+
+        // operation with scalar
+        template <typename T>
+        friend Matrix operator+(T, Matrix);
+        template <typename T>
+        friend Matrix operator+(Matrix, T);
+        template <typename T>
+        void operator+=(T);
+        template <typename T>
+        friend Matrix operator-(T, Matrix);
+        template <typename T>
+        friend Matrix operator-(Matrix, T);
+        template <typename T>
+        void operator-=(T);
+        template <typename T>
+        friend Matrix operator*(T, Matrix);
+        template <typename T>
+        friend Matrix operator*(Matrix, T);
+        template <typename T>
+        void operator*=(T);
+        template <typename T>
+        friend Matrix operator/(T, Matrix);
+        template <typename T>
+        friend Matrix operator/(Matrix, T);
+        template <typename T>
+        void operator/=(T);
 
         // matrix multiplication
-        Matrix matmul(Matrix &);
+        Matrix matmul(Matrix);
 
     private:
         int M;
@@ -62,29 +93,29 @@ class Matrix
 
 // construction from list
 Matrix::Matrix(const initializer_list<initializer_list<float>> &list) {
-    this->M = list.size();
-    this->N = 0;
-    for (int i = 0; i < this->M; i++) {
-        if ((list.begin()+i)->size() > this->N) {
-            this->N = (list.begin()+i)->size();
+    M = list.size();
+    N = 0;
+    for (int i = 0; i < M; i++) {
+        if ((list.begin()+i)->size() > N) {
+            N = (list.begin()+i)->size();
         }
     }
 
-    this->m_data = vector<vector<float>>(this->M, vector<float>(this->N, 0.0));
+    m_data = vector<vector<float>>(M, vector<float>(N, 0.0));
     this->operator=(list);
 }
 
 // assignment from list
 Matrix &Matrix::operator=(const initializer_list<initializer_list<float>> &list) {
     int list_n_rows = list.size();
-    for (int i = 0; i < this->M; i++) {
+    for (int i = 0; i < M; i++) {
         int row_n_elements = (list.begin()+i)->size();
-        for (int j = 0; j < this->N; j++) {
+        for (int j = 0; j < N; j++) {
             if (i < list_n_rows && j < row_n_elements) {
-                this->m_data[i][j] = *((list.begin()+i)->begin()+j);
+                m_data[i][j] = *((list.begin()+i)->begin()+j);
             }
             else {
-                this->m_data[i][j] = 0;
+                m_data[i][j] = 0;
             }
         }
     }
@@ -93,27 +124,36 @@ Matrix &Matrix::operator=(const initializer_list<initializer_list<float>> &list)
 
 // row index
 vector<float> &Matrix::row(int r) {
-    return this->m_data[r];
+    return m_data[r];
 }
 
 // col index
 vector<float> Matrix::col(int c) {
     vector<float> col_data(this->shape()[0], 0.0);
     for (int i = 0; i < this->shape()[0]; i++) {
-        col_data[i] = this->m_data[i][c];
+        col_data[i] = m_data[i][c];
     }
     return col_data;
 }
 
+// transpose
+Matrix Matrix::transpose() {
+    Matrix res(N, M);
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            res[j][i] = m_data[i][j];
+        }
+    }
+    return res;
+}
+
 // concatenate
-Matrix Matrix::cat(Matrix &other, int axis) {
+Matrix Matrix::cat(Matrix other, int axis) {
     if (axis == 0) {
-        int M = this->M;
-        int N = this->N;
         int K = other.shape()[0];
         Matrix res(M+K, N);
 
-        if (this->N != other.shape()[1]) {
+        if (N != other.shape()[1]) {
             cout << "[!] The dimension 1 doesn't match!" << endl;
             return res;
         }
@@ -121,7 +161,7 @@ Matrix Matrix::cat(Matrix &other, int axis) {
         for (int i = 0; i < M + K; i++) {
             for (int j = 0; j < N; j++) {
                 if (i < M) {
-                    res[i][j] = this->m_data[i][j];
+                    res[i][j] = m_data[i][j];
                 }
                 else {
                     res[i][j] = other[i-M][j];
@@ -131,12 +171,10 @@ Matrix Matrix::cat(Matrix &other, int axis) {
         return res;
     }
     else if (axis == 1) {
-        int M = this->M;
-        int N = this->N;
         int K = other.shape()[1];
         Matrix res(M, N+K);
 
-        if (this->M != other.shape()[0]) {
+        if (M != other.shape()[0]) {
             cout << "[!] The dimension 0 doesn't match!" << endl;
             return res;
         }
@@ -144,7 +182,7 @@ Matrix Matrix::cat(Matrix &other, int axis) {
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N + K; j++) {
                 if (j < N) {
-                    res[i][j] = this->m_data[i][j];
+                    res[i][j] = m_data[i][j];
                 }
                 else {
                     res[i][j] = other[i][j-N];
@@ -175,32 +213,32 @@ ostream &operator<<(ostream &out, Matrix mat){
 }
 
 // addition
-Matrix Matrix::operator+(Matrix &other){
+Matrix Matrix::operator+(Matrix other){
     Matrix res(*this);
 
-    if (this->M != other.shape()[0] or this->N != other.shape()[1]) {
+    if (M != other.shape()[0] or N != other.shape()[1]) {
         cout << "[!] The shape of the right matrix doesn't match the left matrix!" << endl;
         return res;
     }
 
-    for (int i = 0; i < this->M; i++) {
-        for (int j = 0; j <this->N; j++) {
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j <N; j++) {
             res[i][j] += other[i][j];
         }
     }
     return res;
 }
 
-void Matrix::operator+=(Matrix &other) {
+void Matrix::operator+=(Matrix other) {
     Matrix res = this->operator+(other);
     this->operator=(res);
 }
 
 // subtraction
-Matrix Matrix::operator-(Matrix &other) {
+Matrix Matrix::operator-(Matrix other) {
     Matrix res(*this);
 
-    if (this->M != other.shape()[0] or this->N != other.shape()[1]) {
+    if (M != other.shape()[0] or N != other.shape()[1]) {
         cout << "[!] The shape of the right matrix doesn't match the left matrix!" << endl;
         return res;
     }
@@ -213,16 +251,16 @@ Matrix Matrix::operator-(Matrix &other) {
     return res;
 }
 
-void Matrix::operator-=(Matrix &other) {
+void Matrix::operator-=(Matrix other) {
     Matrix res = this->operator-(other);
     this->operator=(res);
 }
 
 // element-wise multiplication
-Matrix Matrix::operator*(Matrix &other) {
+Matrix Matrix::operator*(Matrix other) {
     Matrix res(*this);
 
-    if (this->M != other.shape()[0] or this->N != other.shape()[1]) {
+    if (M != other.shape()[0] or N != other.shape()[1]) {
         cout << "[!] The shape of the right matrix doesn't match the left matrix!" << endl;
         return res;
     }
@@ -235,15 +273,155 @@ Matrix Matrix::operator*(Matrix &other) {
     return res;
 }
 
-void Matrix::operator*=(Matrix &other) {
+void Matrix::operator*=(Matrix other) {
     Matrix res = this->operator*(other);
     this->operator=(res);
 }
 
+// element-wise division
+Matrix Matrix::operator/(Matrix other) {
+    Matrix res(*this);
+
+    if (M != other.shape()[0] or N != other.shape()[1]) {
+        cout << "[!] The shape of the right matrix doesn't match the left matrix!" << endl;
+        return res;
+    }
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            res[i][j] /= other[i][j];
+        }
+    }
+    return res;
+}
+
+void Matrix::operator/=(Matrix other) {
+    Matrix res = this->operator/(other);
+    this->operator=(res);
+}
+
+// operator with scalar, scalar add matrix
+template <typename T>
+Matrix operator+(T val, Matrix mat) {
+    Matrix res(mat);
+    for (int i = 0; i < res.shape()[0]; i++) {
+        for (int j = 0; j < res.shape()[1]; j++) {
+            res[i][j] = val + mat[i][j];
+        }
+    }
+    return res;
+}
+
+// operator with scalar, matrix add scalar
+template <typename T>
+Matrix operator+(Matrix mat, T val) {
+    Matrix res(mat);
+    for (int i = 0; i < res.shape()[0]; i++) {
+        for (int j = 0; j < res.shape()[1]; j++) {
+            res[i][j] += val;
+        }
+    }
+    return res;
+}
+
+template <typename T>
+void Matrix::operator+=(T val) {
+    Matrix res = *this + val;
+    this->operator=(res);
+}
+
+// operator with scalar, scalar subtract matrix
+template <typename T>
+Matrix operator-(T val, Matrix mat) {
+    Matrix res(mat);
+    for (int i = 0; i < res.shape()[0]; i++) {
+        for (int j = 0; j < res.shape()[1]; j++) {
+            res[i][j] = val - mat[i][j];
+        }
+    }
+    return res;
+}
+
+// operator with scalar, matrix subtract scalar
+template <typename T>
+Matrix operator-(Matrix mat, T val) {
+    Matrix res(mat);
+    for (int i = 0; i < res.shape()[0]; i++) {
+        for (int j = 0; j < res.shape()[1]; j++) {
+            res[i][j] -= val;
+        }
+    }
+    return res;
+}
+
+template <typename T>
+void Matrix::operator-=(T val) {
+    Matrix res = *this - val;
+    this->operator=(res);
+}
+
+// operator with scalar, scalar multiply matrix
+template <typename T>
+Matrix operator*(T val, Matrix mat) {
+    Matrix res(mat);
+    for (int i = 0; i < res.shape()[0]; i++) {
+        for (int j = 0; j < res.shape()[1]; j++) {
+            res[i][j] = val * mat[i][j];
+        }
+    }
+    return res;
+}
+
+// operator with scalar, matrix multiply scalar
+template <typename T>
+Matrix operator*(Matrix mat, T val) {
+    Matrix res(mat);
+    for (int i = 0; i < res.shape()[0]; i++) {
+        for (int j = 0; j < res.shape()[1]; j++) {
+            res[i][j] *= val;
+        }
+    }
+    return res;
+}
+
+template <typename T>
+void Matrix::operator*=(T val) {
+    Matrix res = *this * val;
+    this->operator=(res);
+}
+
+// operator with scalar, scalar divide matrix
+template <typename T>
+Matrix operator/(T val, Matrix mat) {
+    Matrix res(mat);
+    for (int i = 0; i < res.shape()[0]; i++) {
+        for (int j = 0; j < res.shape()[1]; j++) {
+            res[i][j] = val / mat[i][j];
+        }
+    }
+    return res;
+}
+
+// operator with scalar, matrix divide scalar
+template <typename T>
+Matrix operator/(Matrix mat, T val) {
+    Matrix res(mat);
+    for (int i = 0; i < res.shape()[0]; i++) {
+        for (int j = 0; j < res.shape()[1]; j++) {
+            res[i][j] /= val;
+        }
+    }
+    return res;
+}
+
+template <typename T>
+void Matrix::operator/=(T val) {
+    Matrix res = *this / val;
+    this->operator=(res);
+}
+
 // matrix multiplication, [M x N] * [N x K] = [M x K]
-Matrix Matrix::matmul(Matrix &other) {
-    int M = this->M;
-    int N = this->N;
+Matrix Matrix::matmul(Matrix other) {
     int K = other.shape()[1];
     Matrix res(M, K);
 
@@ -255,7 +433,7 @@ Matrix Matrix::matmul(Matrix &other) {
     vector<float> row;
     vector<float> col;
     for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
+        for (int j = 0; j < K; j++) {
             row = this->row(i);
             col = other.col(j);
             res[i][j] = inner_product(row.begin(), row.end(), col.begin(), 0.0);
